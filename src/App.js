8992548ad70fabc81f09c9a6c6dba48f0006d247
Shubham <dusaneshubham>
2022-11-08 22:33:41 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      Autism Team
-    </div>
+    <Routes>
+      <Route exact path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
