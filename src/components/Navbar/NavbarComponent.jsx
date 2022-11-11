3f7dd0792d64f33ad0ff4d8ac9628d5711f9ac1f
Denis-Shingala <denisshingala@gmail.com>
2022-11-12 00:43:58 +0530

    Navbar Done

--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -1,34 +1,83 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import logoPath from '../../images/logo.png';
+import { Search as SearchIcon, Menu as MenuIcon } from '@mui/icons-material';
+import { Box, SwipeableDrawer, Button, List, Divider, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import './Navbar.css';
 
 const Navbar = () => {
+
+    const [state, setState] = React.useState(false);
+
+    const toggleDrawer = (open) => (event) => {
+        if (
+            event &&
+            event.type === 'keydown' &&
+            (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+            return;
+        }
+        setState(open);
+    };
+
+    const list = () => (
+        <Box
+            sx={{ width: 250 }}
+            role="presentation"
+        >
+            <List>
+                <ListItem>
+                    <form className='navbar-form'>
+                        <input type="search" name="search" className="search-input" placeholder='Search...' />
+                        <button type="submit" name='submit' className='search-btn'><SearchIcon /></button>
+                    </form>
+                </ListItem>
+                <Divider />
+                {['Explore', 'Games', 'Contact Us', 'Pricing'].map((text) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <NavLink to={'/' + text} className='navbar-link'> <ListItemText primary={text} /> </NavLink>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <NavLink to='/Setting' className='navbar-link'> <ListItemText primary='Setting' /> </NavLink>
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
+
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img src={logoPath} alt="autism logo" /></a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Explore</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Game</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link" href="#">Contact us</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Settings</a>
-                            </li>
-                        </ul>
-                    </div>
+            <nav className="nav-bar">
+                {/* <button class='menu-btn' id='menu-btn'><MenuIcon /></button> */}
+                <div className="container-fluid w-100">
+                    <NavLink to="#" className=" navbar-link"><img src={logoPath} alt="Autism Logo" /></NavLink>
+                    <NavLink to="#" className="navbar-link main-navbar-link">Explore</NavLink>
+                    <NavLink to="#" className="navbar-link main-navbar-link">Games</NavLink>
+                    <NavLink to="#" className="navbar-link main-navbar-link">Contact Us</NavLink>
+                    <NavLink to="#" className="navbar-link main-navbar-link">Pricing</NavLink>
+                    <NavLink to="#" className="navbar-link main-navbar-link">Setting</NavLink>
+                    <form className='navbar-form float-end main-navbar-link'>
+                        <input type="search" name="search" className="search-input" placeholder='Search...' />
+                        <button type="submit" name='submit' className='search-btn'><SearchIcon /></button>
+                    </form>
+                    <React.Fragment>
+                        <Button className="menu-btn float-end mx-0 my-2" onClick={toggleDrawer(true)}><MenuIcon /></Button>
+                        <SwipeableDrawer
+                            open={state}
+                            onClose={toggleDrawer(false)}
+                            onOpen={toggleDrawer(true)}
+                            className='box'
+                        >
+                            {list()}
+                        </SwipeableDrawer>
+                    </React.Fragment>
                 </div>
             </nav>
         </>
