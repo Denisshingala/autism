6983acc91fc205bc174ee32029aef9ed906ef1ac
Ankit Verma <ankitverma209171@gmail.com>
2022-11-20 22:44:38 +0530

    added route and dependency

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Routes, Route } from "react-router";
 import Login from "./pages/Login";
 import HomeComponent from "./components/Home/HomeComponent";
+import HandMatchingGameComponent from "./components/HandMatchingGameComponent";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<HomeComponent />} />
       <Route exact path="/login" element={<Login />} />
+      <Route exact path="/game" element={<HandMatchingGameComponent />} />
     </Routes>
   );
 }
