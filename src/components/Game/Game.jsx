ed1fc44a4c0e588cee985c04688904e8414cf7d5
Ankit Verma <ankitverma209171@gmail.com>
2022-11-21 11:55:40 +0530

    added back button component

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,9 +1,10 @@
+import BackButton from "../BackButton";
 import "./Game.css";
 
 export default function Game() {
   return (
     <div className="game">
-      {/* <BackButton /> */}
+      <BackButton />
       <div className="game-header">
         <div className="game-title">Card matching</div>
         <div className="game-timer">Remaining Time: 09:00</div>
